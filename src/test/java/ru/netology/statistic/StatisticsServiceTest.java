13963d632554412ba36702d3578f1ef16aa20d95
Valentin Knyshov <v.a.knishov@bk.ru>
2026-01-17 15:14:46 +0300

    test bad

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,17 +17,17 @@ public class StatisticsServiceTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    void shouldfindMaxequality() {
-        StatisticsService service = new StatisticsService();
-        long[] incomesInBillions = {5, 5, 8, 4, 12, 3, 8, 6, 11, 11, 12};
-        //Исходя из отчета Jococo, не прокрыта тестами строка сервиса,
-        // а именно равенства currentMax = income;,
-        // чтобы покрыть тестами данную строку, поменяем входные данные
-        long expected = 12;
+    // @Test
+    //void shouldfindMaxequality() {
+    //StatisticsService service = new StatisticsService();
+    //long[] incomesInBillions = {5, 5, 8, 4, 12, 3, 8, 6, 11, 11, 12};
+    //Исходя из отчета Jococo, не прокрыта тестами строка сервиса,
+    // а именно равенства currentMax = income;,
+    // чтобы покрыть тестами данную строку, поменяем входные данные
+    //long expected = 12;
 
-        long actual = service.findMax(incomesInBillions);
+    //long actual = service.findMax(incomesInBillions);
 
-        Assertions.assertEquals(expected, actual);
-    }
+    //  Assertions.assertEquals(expected, actual);
+    //}
 }
